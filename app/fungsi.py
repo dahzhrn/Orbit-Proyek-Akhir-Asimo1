45b2f4f48bbdbe43f3e78d3bad768c012ee2bc03
dahzhrn <indahzlbs@gmail.com>
2023-06-05 23:22:21 +0700

    proyek akhir

---
+++ app/fungsi.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.utils import array_to_img
+from tensorflow.keras.layers import Dense , Flatten , GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential
+
+mobilenet = tf.keras.applications.mobilenet.MobileNet(input_shape=(32 , 32, 3),
+                                           include_top=False,
+                                           weights='imagenet')
+
+def make_model():
+    model = Sequential()
+    model.add(mobilenet)
+    model.add(GlobalAveragePooling2D())
+    model.add(Flatten())
+    model.add(Dense(1024, activation="relu"))
+    model.add(Dense(512, activation="relu"))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dense(7, activation="softmax" , name="classification"))
+
+    return model
